de077871e4a130cb690c2aedc34f918956386349
jallinhabbei12434 <pegaasala923@gmail.com>
2025-05-29 00:57:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    numero = data.get("phone")
-    print(f"Recebido: {numero}")
-    return {"status": "ok"}
 
 @app.route('/')
 def home():
     return 'Flask está rodando.'
+
+@app.route('/webhook', methods=['POST'])
+def receber_webhook():
+    dados = request.get_json()
+    print('📥 Dados recebidos do n8n:')
+    print(dados)
+
+    # Aqui você pode salvar, processar ou responder
+    return jsonify({'mensagem': 'Recebido com sucesso'}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
